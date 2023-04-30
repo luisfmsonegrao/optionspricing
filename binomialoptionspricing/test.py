ecbd08dc4b540b748036cd107077d971b9d4071e
Luís <luis.fmso.negrao@gmail.com>
2023-04-30 19:54:42 +0200

    Fixed import statement in test.py

--- binomialoptionspricing/test.py
+++ binomialoptionspricing/test.py
@@ -1,7 +1,6 @@
-from .binomialoptionpricing.py import BinomialOptionTree
+from .binomialoptionpricing import BinomialOptionTree
 
 def test_binomial_option_tree():
     bop1 = BinomialOptionTree(100, 100, "Call", 1.01, 0.99, 0.05, 4, 1)
     assert bop1.stock_price == 100
 
-    
