690656d23d6a0fdcffef5b4c3e40f3ddcf806dfe
Luís <luis.fmso.negrao@gmail.com>
2023-05-06 13:37:13 +0200

    Fixed syntax errors

--- binomialoptionspricing/test.py
+++ binomialoptionspricing/test.py
@@ -1,6 +1,6 @@
 from .binomialoptionpricing import BinomialOptionTree
 
-def create_option_tree()
+def create_option_tree():
     return BinomialOptionTree(100, 100, "Call", 1.01, 0.99, 0.05, 4, 1)
     
 
@@ -15,7 +15,7 @@ def test_binomial_option_tree_constructor():
     assert bop1.n_steps == 4
     assert bop1.step == 1
 
-def test_calculate_risk_free_probability()
+def test_calculate_risk_free_probability():
     bop = create_option_tree()
     assert bop._calculate_risk_free_probability() = bop.risk_free_probability
 
