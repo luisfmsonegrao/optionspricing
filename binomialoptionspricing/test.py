31651c358f1f90effaac88e20aa50ac97c67cabd
Luís <luis.fmso.negrao@gmail.com>
2023-05-06 13:34:30 +0200

    Added new test

--- binomialoptionspricing/test.py
+++ binomialoptionspricing/test.py
@@ -1,7 +1,11 @@
 from .binomialoptionpricing import BinomialOptionTree
 
-def test_binomial_option_tree():
-    bop1 = BinomialOptionTree(100, 100, "Call", 1.01, 0.99, 0.05, 4, 1)
+def create_option_tree()
+    return BinomialOptionTree(100, 100, "Call", 1.01, 0.99, 0.05, 4, 1)
+    
+
+def test_binomial_option_tree_constructor():
+    bop1 = create_option_tree()
     assert bop1.stock_price == 100
     assert bop1.strike_price == 100
     assert bop1.option_type == "Call"
@@ -9,6 +13,12 @@ def test_binomial_option_tree():
     assert bop1.down_move == 0.99
     assert bop1.rate == 0.05
     assert bop1.n_steps == 4
-    assert bop1.step == 2
+    assert bop1.step == 1
+
+def test_calculate_risk_free_probability()
+    bop = create_option_tree()
+    assert bop._calculate_risk_free_probability() = bop.risk_free_probability
+
+
     
 
